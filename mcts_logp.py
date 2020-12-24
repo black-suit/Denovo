0845655a5c6ec042eaff8839e0ceed2e3ba2d7a6
black suit <59173753+black-suit@users.noreply.github.com>
2020-12-24 20:49:09 +0900

    Create mcts_logp.py

---
+++ mcts_logp.py
@@ -0,0 +1,18 @@
+import numpy as np
+import time
+import math
+import random as pr
+
+from load_model import loaded_model
+from add_node_type import chem_kn_simulation, expanded_node, node_to_add, predict_smile, make_input_smile, check_node_type
+from make_smile import zinc_data_with_bracket_original, zinc_processed_with_bracket
+
+class chemical:
+
+class Node:
+
+def MCTS(root,verbose = False):
+
+def UCTchemical():
+
+if __name__ == "__main__":
