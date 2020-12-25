c1357477eaf1563dfe42d1fa4cf8c9582ca23d16
black suit <59173753+black-suit@users.noreply.github.com>
2020-12-26 06:25:54 +0900

    Update mcts_logp.py

--- mcts_logp.py
+++ mcts_logp.py
@@ -14,5 +14,24 @@ class Node:
 def MCTS(root,verbose = False):
 
 def UCTchemical():
+  one_search_start_time = time.time()
+  time_out = one_search_start_time + 60*10
+  state = chemical()
+  best = MCTS(root=state,verbose=False)
+  
+  return best
 
 if __name__ == "__main__":
+  smile_old = zinc_data_with_bracket_original()
+  val,smile = zinc_processed_with_bracket(smile_old)
+  
+  logP_values = np.loadtxt('logP_values.txt')
+  SA_scores = np.loadtxt('SA_scores.txt')
+  #cycle_scores = np.loadtxt('cycle_scores.txt')
+  SA_mean = np.mean(SA_scores)
+  SA_std = np.std(SA_scores)
+  logP_mean = np.mean(logP_values)
+  #cycle_mean = np.mean(cycle_scores)
+  #cycle_std = np.std(cycle_scores)
+  model = loaded_model()
+  valid_compound = UCTChemical()
